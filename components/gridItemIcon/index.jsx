19480eff2d78fbfa0ebad79cd5dcb443d065ee4f
Timon Forrer <timon@Timons-MacBook-Pro.local>
2019-11-03 17:11:08 +0100

    Deprecated

--- components/gridItemIcon/index.jsx
+++ components/gridItemIcon/index.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-
-import IconLoader from '../../components/iconLoader'
-
 import './styles.scss'
+import Icon from '../icon'
 
 const GridItemIcon = ({icon, children}) => {
   return (
     <div>
       <div className="grid__item grid__item--icon">
         <div>
-          <IconLoader width={32} height={32} icon={icon} circle />
+          {/* <IconLoader width={32} height={32} icon={icon} circle /> */}
+          <Icon name={icon} width={32} height={32} circle color="white" />
         </div>
         <div>{children}</div>
       </div>
